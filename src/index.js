d0b93565e49d140202723de30159dbc27d19cfab
nesnip <hpmarines12@gmail.com>
2020-01-28 19:11:20 -0500

    add test and functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-/* eslint-disable no-unused-vars */
-function isAbsolute(path) {
-  if (path === '') {
-    return true;
-  }
-  return false;
-}
-module.exports = isAbsolute;
+const path = require('path');
+const fs = require('fs');
 
-console.log('ruta');
+const currentPath = path.resolve();
+
+const isAbsolute = (str) => path.isAbsolute(str);
+
+const toAbsolute = (relative) => path.resolve(relative);
+
+const isDirectory = (str) => fs.statSync(str).isDirectory();
+
+const isMdFile = (str) => path.extname(str) === '.md';
+
+const allPaths = (str) => fs.readdir(str, (err, files) => {
+  if (err) {
+    console.log(`Unable to scan directory: ${err}`);
+  }
+
+  files.forEach((file) => (fs.statSync(file).isFile());
+});
+
+console.log(currentPath);
+console.log(isDirectory(currentPath));
+console.log(allPaths(currentPath));
+
+console.log(fs.readdirSync('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links'));
+
+module.exports = {
+  isAbsolute,
+  toAbsolute,
+  isDirectory,
+  isMdFile,
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,64 @@
-const isAbsolute = require('../src/index');
+const myFunctions = require('../src/index');
+
+const {
+  isAbsolute, toAbsolute, isDirectory, isMdFile,
+} = myFunctions;
 
 describe('isAbsolute', () => {
-  it('Debería ser una función', () => {
+  it('Debería de ser una función', () => {
     expect(typeof isAbsolute).toBe('function');
   });
   it('Es una función que determina cuando una ruta es absoluta', () => {
-    const input = '';
+    const input = '/LIM011-fe-md-links/';
     const output = true;
     expect(isAbsolute(input)).toEqual(output);
   });
-  it('Es una función que determina cuando una ruta no es absoluta', () => {
+  it('Es una función que determina cuando una ruta NO es absoluta', () => {
     const input = '../src/index.js';
     const output = false;
     expect(isAbsolute(input)).toEqual(output);
+  });
+});
+
+describe('toAbsolute', () => {
+  it('Debería de ser una función', () => {
+    expect(typeof toAbsolute).toBe('function');
+  });
+  it('Es una función que convierte una ruta realtiva en absoluta', () => {
+    const input = './baz';
+    const output = '/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/baz';
+    expect(toAbsolute(input)).toEqual(output);
+  });
+});
+
+describe('isDirectory', () => {
+  it('Debería de ser una función', () => {
+    expect(typeof isDirectory).toBe('function');
+  });
+  it('Es una función que determina cuando una ruta es un directorio', () => {
+    const input = '/home/marines/Escritorio/Laboratoria';
+    const output = true;
+    expect(isDirectory(input)).toEqual(output);
+  });
+  it('Es una función que determina cuando una ruta NO es un directorio', () => {
+    const input = '/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/src/index.js';
+    const output = false;
+    expect(isDirectory(input)).toEqual(output);
+  });
+});
+
+describe('isMdFile', () => {
+  it('Debería de ser una función', () => {
+    expect(typeof isMdFile).toBe('function');
+  });
+  it('Es una función que determina si el archivo tiene formato .md', () => {
+    const input = 'README.md';
+    const output = true;
+    expect(isMdFile(input)).toEqual(output);
+  });
+  it('Es una función que determina si el archivo NO tiene formato .md', () => {
+    const input = 'index.js';
+    const output = false;
+    expect(isMdFile(input)).toEqual(output);
   });
 });
