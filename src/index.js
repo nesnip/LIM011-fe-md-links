a5a9c4a33696d66a4091c94d0952657f8da8fc95
nesnip <hpmarines12@gmail.com>
2020-01-31 20:05:40 -0500

    add getlinks function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable max-len */
 const path = require('path');
 const fs = require('fs');
+const marked = require('marked');
 
 // const currentPath = path.resolve();
 // const directoryPath = path.join(__dirname);
@@ -30,10 +32,37 @@ const recursion = (ruta) => {
 };
 
 const readFile = (str) => fs.readFileSync(str, 'utf8');
+// console.log(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'));
 
-const readlink = (str) => ;
+const getLinks = (mdFile) => {
+  const renderer = new marked.Renderer();
+  const links = [];
+  renderer.link = (href, title, text) => {
+    links.push(`${href}, ${text}, '/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'`);
+  };
+  marked(mdFile, { renderer });
+  return links;
+  /* renderer.link = (href, title, text) => {
+    value = value.concat(`${href}, ${title}, ${text}`);
+    console.log(value);
+    return `{${href}, ${title}, ${text}}`;
+  }; */
+};
+console.log(getLinks(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md')));
 
-console.log(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'));
+/* const renderer = new marked.Renderer();
+renderer.link = (href, title, text) => {
+  let value = [];
+  value = value.concat(`${href}, ${title}, ${text}`);
+  console.log(value);
+  return `{${href}, ${title}, ${text}}`;
+}; */
+
+// const tokens = marked.lexer(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'));
+// console.log(tokens);
+
+// console.log(marked(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'), { renderer }));
+// console.log(readFile('/home/marines/Escritorio/Laboratoria/MD LINKS/LIM011-fe-md-links/README.md'));
 
 module.exports = {
   isAbsolute,
